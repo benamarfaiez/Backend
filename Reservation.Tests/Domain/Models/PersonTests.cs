49205224f2a28566fa95dbdc60b5f47481a712b5
faiez_benamar <38057044+benamarfaiez@users.noreply.github.com>
2025-03-28 00:34:40 +0100

    Fix test unitaire Shoud_Create_Person

--- Reservation.Tests/Domain/Models/PersonTests.cs
+++ Reservation.Tests/Domain/Models/PersonTests.cs
@@ -8,14 +8,10 @@ namespace Reservation.Tests.Domain.Models
         [Fact]
         public void Shoud_Create_Person()
         {
-            const int id = 100;
-            const int idRoom = 100;
-            const int idPerson = 100;
-
             var value = new Person(1,"Fred","Martin");
 
             Check.That(value.FirstName).Is("Fred");
-            Check.That(value.FirstName).Is("Martin");
+            Check.That(value.LastName).Is("Martin");
         }
     }
 }
